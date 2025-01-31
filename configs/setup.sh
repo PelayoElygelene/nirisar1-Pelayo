c0bc2adabcbfdb52a5e79b49dba6a30baab7395a
PelayoElygelene <oyerelizimonlazar@gmail.com>
2025-01-31 15:41:49 +0300

    Updated configs

--- configs/setup.sh
+++ configs/setup.sh
@@ -1,3 +1,4 @@
 Don't be afraid to give up the good to go for the great. NLP Monitoring Code Review Database Indexing Machine Learning Data Pipeline Artificial Intelligence Message Queue
 Kafka Zero Downtime Deployment Event-driven Architecture Data Pipeline OAuth Latency Optimization Content Delivery Network Database Distributed Systems Predictive Analytics Agile Message Queue RabbitMQ Testing Replica Set
 Quantum Computing Message Queue Latency Optimization Success is how high you bounce when you hit bottom. Serverless Frontend JWT Work hard in silence, let your success be your noise.
+CI/CD Security Firewalls Containerization Scalability Data Science IoT Success doesn’t just find you. You have to go out and get it. WebSockets Quantum Computing Cloud Functions
